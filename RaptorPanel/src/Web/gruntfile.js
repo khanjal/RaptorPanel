3758ebdcfa27e95ae4eb960b106764985bb3cdf0
khanjal <khanjal@gmail.com>
2015-03-28 18:33:26 -0400

    Adding additional files that weren't included in the first commit.

---
+++ RaptorPanel/src/Web/gruntfile.js
@@ -0,0 +1,39 @@
+﻿// This file in the main entry point for defining grunt tasks and using grunt plugins.
+// Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: "wwwroot/lib",
+                    layout: "byComponent",
+                    cleanTargetDir: false
+                }
+            }
+        },
+
+        uglify: {
+            my_target: {
+                files: { 'wwwroot/app.js': ['Scripts/app.js', 'Scripts/**/*.js'] }
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['Scripts/**/*.js'],
+                tasks: ['uglify']
+            }
+        }
+    });
+
+    // This command registers the default task which will install bower packages into wwwroot/lib
+    grunt.registerTask("default", ["bower:install"]);
+    grunt.registerTask('default', ['uglify', 'watch']);
+
+    // The following line loads the grunt plugins.
+    // This line needs to be at the end of this this file.
+    grunt.loadNpmTasks("grunt-bower-task");
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
